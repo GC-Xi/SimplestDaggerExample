60f2b030b69718b5cb33404afcafff024f2e2688
GC-Xi <xi@gc.com>
2018-10-02 20:44:49 -0400

    Step 4: Using the injection.

--- app/src/main/java/com/xi_zz/simplestdaggerexample/MainActivity.kt
+++ app/src/main/java/com/xi_zz/simplestdaggerexample/MainActivity.kt
@@ -2,11 +2,19 @@ package com.xi_zz.simplestdaggerexample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.myTextView
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject lateinit var info: Info
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        DaggerAppComponent.builder().build().inject(this)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myTextView.text = info.text
     }
 }
