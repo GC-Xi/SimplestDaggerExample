9f11a7f87418e199a15f0ad2a5fba2811202b212
GC-Xi <xi@gc.com>
2018-10-06 17:41:50 -0400

    Fixed typo

--- app/src/main/java/com/xi_zz/simplestdaggerexample/AppComponent.kt
+++ app/src/main/java/com/xi_zz/simplestdaggerexample/AppComponent.kt
@@ -6,10 +6,10 @@ import javax.inject.Singleton
 
 // The Component is declaring what and where to inject.
 // A Component should always be an interface.
-// Dagger will generate a class Add `Dagger` in front of the component name. DaagerAppComponent in this case.
+// Dagger will generate a class Add `Dagger` in front of the component name, DaggerAppComponent in this case.
 @Singleton
 @Component
 interface AppComponent {
-    // You can name it anything, such as `poke()`. It doesn't have to be `inject()`
+    // You can name it anything, such as `poke()`. It doesn't have to be `inject()`.
     fun inject(activity: MainActivity)
 }
