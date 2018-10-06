e62bcd03b7404a4f3d58a1209fdf4ebb0d1e072a
GC-Xi <xi@gc.com>
2018-10-06 17:41:50 -0400

    Fixed typo

--- app/src/main/java/com/xi_zz/simplestdaggerexample/AppComponent.kt
+++ app/src/main/java/com/xi_zz/simplestdaggerexample/AppComponent.kt
@@ -6,7 +6,7 @@ import javax.inject.Singleton
 
 // The Component is declaring what and where to inject.
 // A Component should always be an interface.
-// Dagger will generate a class Add `Dagger` in front of the component name. DaagerAppComponent in this case.
+// Dagger will generate a class Add `Dagger` in front of the component name. DaggerAppComponent in this case.
 @Singleton
 @Component
 interface AppComponent {
